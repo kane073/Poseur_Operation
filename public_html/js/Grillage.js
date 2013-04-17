f03bfe3a38110c17ef91ad1ab2a62e55e706b132
Jeremy Sfez <sfez_555@hotmail.com>
2013-04-17 15:40:15 +0200

    Hack clavier 3

--- public_html/js/Grillage.js
+++ public_html/js/Grillage.js
@@ -696,8 +696,12 @@ function Grillage(idDiv) {
             //effacerContour(listeErreurSimple[i].x, listeErreurSimple[i].y);
         }
     }
+<<<<<<< HEAD
 
     var coordonneGrilleCourant;
+=======
+	
+>>>>>>> Hack clavier 3
     /**
      * Cette methode correspond à l'évenement faite quand on click dans la grille
      * @param {type} event
