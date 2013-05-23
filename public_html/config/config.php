cfad4d5c62ca38d64629c0c6a2cc3a132c53e787
Alassane <alassane.kane7@gmail.com>
2013-05-23 11:31:15 +0200

    Version finale2

--- public_html/config/config.php
+++ public_html/config/config.php
@@ -2,12 +2,15 @@
 
 if (isset($_POST['configuration'])) {
     $configuration = $_POST['configuration'];
-    $fp = fopen('config.json', 'w');
-    fwrite($fp, $configuration);
+	$data = "var donnees_json = '".$configuration."';";
+    $fp = fopen('config2.js', 'w');
+    fwrite($fp, $data);
     fclose($fp);
     $reponse = array("reponse" => "success");
-    echo json_encode($reponse);
+    echo json_decode($reponse);
 } else {
     $reponse = array("reponse" => "error");
-    echo json_encode($reponse);
+    echo json_decode($reponse);
 }
+
+?>
